c1edb0c6f298aa2ea2f145557daec635374901de
unknown <thompson.jonm@gmail.com>
2017-07-19 12:43:19 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# liri-app
+# Language Interpretation and Recognition Interface (LIRI)
+
+# What is LIRI?
+LIRI is a command line node.js application which takes in user parameters and provides data.
+
+# Setup
+
+Work in progress.
---
+++ keys.js
@@ -0,0 +1,12 @@
+//keys go here
+exports.twitterKeys = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '-',
+  access_token_secret: '',
+}
+
+exports.spotifyKeys = {
+  clientId_key: '',
+  clientSecret_key: '',
+}

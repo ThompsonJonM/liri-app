4844cd354fde429b9a8bd386c448aeb2fcb9b1c5
unknown <thompson.jonm@gmail.com>
2017-07-20 14:23:44 -0500

    keys added

--- keys.js
+++ keys.js
@@ -1,12 +1,13 @@
-//keys go here
+// twitter keys
 exports.twitterKeys = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '-',
-  access_token_secret: '',
+  consumer_key: 'EnSGvxwrBmXNaHttEwffKPQG1',
+  consumer_secret: 'NLnu1aB9fLu4EES60q7PqUGKKpSvIX80hNRNTdQrz25nkGuJJL',
+  access_token_key: '888115991816699909-q17JThealbq35oPHOZ0aMUDO6a3cd1A',
+  access_token_secret: 'Vcc3hLdtmn7rtQuxujD5NxcmLUCJ0mWwc8JSARWl8s3C6',
 }
 
+// spotify keys
 exports.spotifyKeys = {
-  clientId_key: '',
-  clientSecret_key: '',
+  clientId_key: '71189957d75445bf8076eca7b736552e',
+  clientSecret_key: 'a27ba313138249acbff71521ac69d0e9',
 }

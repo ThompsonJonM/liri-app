67d3967feaae39a73067915dfb22dbea4cdc45f0
unknown <thompson.jonm@gmail.com>
2017-07-20 14:09:22 -0500

    variables

--- liri.js
+++ liri.js
@@ -1,1 +1,13 @@
-//placeholder
+// node.js variables
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fileSystem = require("fs");
+var request = require("request");
+
+// keys
+var client = new Twitter({
+    consumer_key: 
+})
+
+// omdb
+var omdbURL = "http://www.omdbapi.com/?apikey=40e9cece&t=";

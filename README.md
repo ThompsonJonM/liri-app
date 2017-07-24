b4be78367ba5befeb39722fb59aff5965b583bd2
thompsonjonm <thompson.jonm@gmail.com>
2017-07-23 22:50:33 -0500

    final state

--- README.md
+++ README.md
@@ -8,25 +8,26 @@ LIRI is a command line node.js application which takes in user parameters and pr
 * Run NPM install
     + This should install the following packages:
         * Twitter
-        * Spotify
+        * Node-Spotify-API
         * Request
+        * Filesystem
 
 # Available Parameters
 LIRI comes with four parameters out of the box. They are:
-* spotifyMe
-* omdbMe
-* tweetMe
+* spotify-this-song
+* movie-this
+* my-tweets
 * do-what-it-says
 
 # Using LIRI
 
 LIRI functions by using a simple set of commands. Each command will appear as the following:
 * node liri < function > < parameter > ( see above for a listing of parameters )
-    + Spotify: node liri spotifyMe "song, artist, etc"
-    + omdb: node liri omdbMe "title, actor, etc"
+    + Spotify: node liri spotify-this-song "song, artist, etc"
+    + omdb: node liri movie-this "title, actor, etc"
         * If the input is more than one word (eg. The Guardian) write the full input with spaces substituted with "+".
         * For example: "the+guardian" and "bruce+willis"
-    + Twitter: node liri tweetMe 
+    + Twitter: node liri my-tweets 
         * This is the only node command which will not require a parameter input
         * Please make sure not to write anything after "tweetMe"
     + do-what-it-says: node liri do-what-it-says
--- liri.js
+++ liri.js
@@ -9,6 +9,19 @@ var fileSystem = require("fs");
 var request = require("request");
 var keys = require("./keys.js")
 
+// log
+fileSystem.appendFile("log.txt", "\n" + process.argv[0] + "\n" + process.argv[1] + "\n" + process.argv[2] + "\n" + process.argv[3] + "\n", function (err) {
+    if (inputOne === "my-tweets") {
+        fileSystem.appendFile("log.txt", "\n" + process.argv[0] + "\n" + process.argv[1] + "\n" + process.argv[2] + "\n", function (err) {
+            if (err) throw err;
+        })
+    }
+
+    if (err) throw err;
+
+    console.log("Log Saved!");
+});
+
 // keys
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
@@ -49,9 +62,8 @@ function omdbMe(body) {
     console.log("\n--------------------------------------------------------");
     console.log('Title:' + ' ' +omdbBody.Title);
     console.log('Year:' + ' ' +omdbBody.Year);
-    // must fix
-    //console.log('Rating:' + ' ' +omdbBody.Ratings[0].Source + ':' + ' ' + obj.Ratings[0].Value );
-    //console.log('Rating:' + ' ' +omdbBody.Ratings[1].Source + ':' + ' ' + obj.Ratings[1].Value );
+    console.log('Rating:' + ' ' +omdbBody.Ratings[0].Source + ':' + ' ' + omdbBody.Ratings[0].Value );
+    console.log('Rating:' + ' ' +omdbBody.Ratings[1].Source + ':' + ' ' + omdbBody.Ratings[1].Value );
     console.log('Country:' + ' ' +omdbBody.Country);
     console.log('Language:' + ' ' +omdbBody.Language);
     console.log('Plot:' + ' ' +omdbBody.Plot);
@@ -61,33 +73,19 @@ function omdbMe(body) {
 }
 
 if (!inputOne){
-    console.log("LIRI requires a function input to operate.");
+    console.log("LIRI requires a function input to operate. Please input a function command.");
     return;
 }
 
 switch(inputOne) {
-  
-    case 'tweetMe':
-        if(inputOne && inputTwo){
-            console.log("Please do not enter a parameter after tweetMe.");
-            return;
-        }
-        client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', function(error, tweets, response) {
-            tweets.forEach(function(element) {
-            console.log("\n--------------------------------------------------------");
-            console.log(element.created_at + '     ' +  element.text);
-            console.log("\n--------------------------------------------------------");
-            console.log("\n");
-        });
-    });
-    break;
     
-    case 'spotifyMe':
+    case 'spotify-this-song':
         if (!inputTwo){
-            spotify.search({ type: 'track', query: 'Thrice'}, function(err, data) {
+            spotify.search({ type: 'track', query: 'ace+of+base'}, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+
         console.log("\n--------------------------------------------------------");
         console.log("Artist(s):" + ' ' + data.tracks.items[0].album.artists[0].name);
         console.log("Song Name:" + ' ' + data.tracks.items[0].name);
@@ -95,15 +93,18 @@ switch(inputOne) {
         console.log("Album:" + ' ' + data.tracks.items[0].album.name);
         console.log("\n--------------------------------------------------------");
         console.log("\n");
+        
         });
-        return;
+            return;
         }
+
         spotifyMe();
-        break;
+
+    break;
     
-    case 'omdbMe':
+    case 'movie-this':
         if (!inputTwo){
-            request(omdbURL + 'Terminator', function (error, response, body){
+            request(omdbURL + 'mr+nobody', function (error, response, body){
                 omdbMe(body);
         });
             return;
@@ -112,19 +113,37 @@ switch(inputOne) {
         request(omdbURL + inputTwo, function (error, response, body){
             omdbMe(body);
         });
-        break;
+        
+    break;
+
+    case 'my-tweets':
+        if(inputOne && inputTwo){
+            console.log("Please do not enter a parameter after my-tweets.");
+            return;
+        }
+
+        client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', function(error, tweets, response) {
+            tweets.forEach(function(element) {
+            console.log("\n--------------------------------------------------------");
+            console.log(element.created_at + '     ' +  element.text);
+            console.log("\n--------------------------------------------------------");
+            console.log("\n");
+            });
+        });
+
+    break;
     
     case 'do-what-it-says':
         fileSystem.readFile('./random.txt','utf8', function(err, data){
             var dataArr = data.split(',');
             inputTwo = dataArr[1];
             
-            if (dataArr[0] === "spotifyMe") {
+            if (dataArr[0] === "spotify-this-song") {
                 spotifyMe(dataArr[1]);
-            } else if (dataArr[0] === "omdbMe") {
+            } else if (dataArr[0] === "movie-this") {
                 omdbMe(dataArr[1]);
             }
         }); 
 
-        break;
+    break;
 }
